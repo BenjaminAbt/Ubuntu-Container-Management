1b07cee929d1ad2d684ce1bb2cad4414e8cc6595
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 19:05:10 +0100

    Update helm-install-traefik.sh

--- helm-install-traefik.sh
+++ helm-install-traefik.sh
@@ -1,3 +1,3 @@
 SERVICETYPE="$1"
 EXTERNALIP="$2"
-helm install stable/traefik --name traefik --namespace kube-system --set serviceType="$SERVICETYPE"
+helm install stable/traefik --name traefik --namespace kube-system --set serviceType="$SERVICETYPE",externalIP="$EXTERNALIP"
