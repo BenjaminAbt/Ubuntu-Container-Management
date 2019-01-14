15da0a55cb0da1d6d5b0f815b2613dfffe6b31af
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 15:32:34 +0100

    Update ubuntu-install-docker.sh

--- ubuntu-install-docker.sh
+++ ubuntu-install-docker.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
 
+# Use this script only against a fresh ubuntu server
+#    curl https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-install-docker.sh | sudo bash
+
+# install docker
+snap install docker
