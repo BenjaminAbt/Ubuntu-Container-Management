eae48b4de3b0c28a92ae60d9d79368daebc33c0b
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 19:11:47 +0100

    Delete ubuntu-install-docker.sh

--- ubuntu-install-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Use this script only against a fresh ubuntu server
-#    curl https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-install-docker.sh | sudo bash
-
-# install docker
-snap install docker
