e5b37f3b0c045f4a40fd6d8844af5ce954a02f3d
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 21:51:49 +0100

    Update ubuntu-setup-kubernetes.sh

--- ubuntu-setup-kubernetes.sh
+++ ubuntu-setup-kubernetes.sh
@@ -3,9 +3,6 @@
 # Use this script only against a fresh ubuntu server
 #    curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-setup-kubernetes | sudo bash
 
-# use weave network
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
 # init kube admin
 kubeadm init
 
@@ -16,6 +13,9 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # allow pods on master, otherwise helm tiller and pods will not start
 kubectl taint nodes --all node-role.kubernetes.io/master-
+
+# use weave network
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # re-config helm
 kubectl -n kube-system create serviceaccount tiller
