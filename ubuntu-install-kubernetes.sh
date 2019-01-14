344b941f4c52b6effd7a4d235e6484fad08ef8de
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 19:16:49 +0100

    Update ubuntu-install-kubernetes.sh

--- ubuntu-install-kubernetes.sh
+++ ubuntu-install-kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Use this script only against a fresh ubuntu server
-#    curl https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-install-kubernetes.sh | sudo bash
+#    curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-install-kubernetes.sh | sudo bash
 
 
 # update package index and install https apt
