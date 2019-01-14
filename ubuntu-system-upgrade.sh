92dbdd702d4fb19cb2fa9ae9d5228611d7ee7fa5
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 16:23:31 +0100

    Update ubuntu-system-upgrade.sh

--- ubuntu-system-upgrade.sh
+++ ubuntu-system-upgrade.sh
@@ -3,5 +3,6 @@
 # Use this script only against a fresh ubuntu server
 #    curl -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-system-upgrade.sh | sudo bash
 # requires a reboot (sudo reboot)
+
 sudo apt update -y
 sudo apt upgrade -y
