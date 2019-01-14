6fe9011571681c43e17d11c6bf43fcbbf38d7901
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 16:16:01 +0100

    Update ubuntu-system-upgrade.sh

--- ubuntu-system-upgrade.sh
+++ ubuntu-system-upgrade.sh
@@ -3,5 +3,5 @@
 # Use this script only against a fresh ubuntu server
 #    curl https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-system-upgrade.sh | sudo bash
 
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
