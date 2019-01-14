3feb3a9eabe2a1f60d3716758ea328d4556ac983
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-14 15:41:15 +0100

    Create ubuntu-system-upgrade.sh

---
+++ ubuntu-system-upgrade.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Use this script only against a fresh ubuntu server
+#    curl https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-system-upgrade.sh | sudo bash
+
+sudo apt update
+sudo apt upgrade
