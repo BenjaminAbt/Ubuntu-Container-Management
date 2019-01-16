007f0e51e6e6053d1eac88b9376aca4a41bf09c5
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-16 22:10:52 +0100

    Delete ubuntu-install-hostess.sh

--- ubuntu-install-hostess.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/ubuntu-install-hostess.sh | sudo bash
-
-git clone https://github.com/cbednarski/hostess
-cd hostess
-make
-make install
