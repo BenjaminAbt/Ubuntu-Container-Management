4f796a90e1b75d945a18a3d555ef9ca90616a7a2
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-16 21:18:04 +0100

    Create helm-rancher-install.sh

---
+++ helm-rancher-install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/helm-rancher-install.sh | sudo bash -s <address>
+
+ADDRESS="$1"
+helm install rancher-latest/rancher --name rancher --namespace cattle-system --set hostname="$ADDRESS" --wait
