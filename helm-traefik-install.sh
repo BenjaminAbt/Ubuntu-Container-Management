0ed1d7ee8fa32e2180a611fa6d4473853733e04f
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-16 21:18:01 +0100

    Update helm-traefik-install.sh

--- helm-traefik-install.sh
+++ helm-traefik-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/helm-traefik-install.sh | sudo bash
+# curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/helm-traefik-install.sh | sudo bash -s NodePort 1.2.3.4
 
 SERVICETYPE="$1"
 EXTERNALIP="$2"
