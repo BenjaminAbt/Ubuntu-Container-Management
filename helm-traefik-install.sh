6055b2b95aaa4db18194da49eeadc5792f1832bd
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2019-01-16 21:16:39 +0100

    Update helm-traefik-install.sh

--- helm-traefik-install.sh
+++ helm-traefik-install.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# curl -sS -H "Cache-Control: no-cache" https://raw.githubusercontent.com/BenjaminAbt/Ubuntu-Kubernetes/master/helm-traefik-install.sh | sudo bash
+
 SERVICETYPE="$1"
 EXTERNALIP="$2"
 helm install stable/traefik --name traefik --namespace kube-system --set serviceType="$SERVICETYPE",externalIP="$EXTERNALIP"
